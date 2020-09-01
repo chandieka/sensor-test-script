75171e7bc7217cf1d1d5122af0e8a14cf5d53bb3
chandieka <candiekatasbihyantra2@gmail.com>
2020-09-01 15:06:52 -0400

    update

--- mon.sh
+++ mon.sh
@@ -32,11 +32,11 @@ do
     rxbytes_new3="`cat /sys/class/net/$int3/statistics/rx_bytes`";
     rxbytes_new4="`cat /sys/class/net/$int4/statistics/rx_bytes`";
     
-    rxbytes_old_sum=$(( $rxbytes_old1 + $rxbytes_old2 + $rxbytes_old3 + $rxbytes_old4 ))
-    txbytes_old_sum=$(( $txbytes_old1 + $txbytes_old2 + $txbytes_old3 + $txbytes_old4 ))
+    rxbytes_old_sum=$(( rxbytes_old1 + rxbytes_old2 + rxbytes_old3 + rxbytes_old4 ))
+    txbytes_old_sum=$(( txbytes_old1 + txbytes_old2 + txbytes_old3 + txbytes_old4 ))
 
-    rxbytes_new_sum=$(( $rxbytes_new1 + $rxbytes_new2 + $rxbytes_new3 + $rxbytes_new4 ))
-    txbytes_new_sum=$(( $txbytes_new1 + $txbytes_new2 + $txbytes_new3 + $txbytes_new4 ))
+    rxbytes_new_sum=$(( rxbytes_new1 + rxbytes_new2 + rxbytes_new3 + rxbytes_new4 ))
+    txbytes_new_sum=$(( txbytes_new1 + txbytes_new2 + txbytes_new3 + txbytes_new4 ))
 
     txbytes=$(( txbytes_new_sum - txbytes_old_sum));
     rxbytes=$(( rxbytes_new_sum - rxbytes_old_sum));
