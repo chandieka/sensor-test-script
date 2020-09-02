46685c318fa3938fce146a98856dc7c0050187dd
chandieka <candiekatasbihyantra2@gmail.com>
2020-09-02 04:02:42 -0400

    update

---
+++ drop.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+drop() {
+    capPacket=$(cat /nsm/sensor_datavirtualsocdemo-sensor-$1/stats.log | grep capture | tail -n 1 | cut -d '|' -f3);
+    dropPacket=$(cat /nsm/sensor_datavirtualsocdemo-sensor-$1/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
+    percentageDrop=$( echo "scale=2; ($dropPacket*100)/($dropPacket + $capPacket)" | bc )
+
+    echo ""
+    echo "====================="
+    echo ""
+    echo "Interface: $1"
+    echo "Capture Packet: $capPacket"
+    echo "Drop Packet: $dropPacket"
+    echo "Percentage drop overlifetime: $percentageDrop"
+    echo ""
+    echo "====================="
+    echo ""
+}
+
+drop eth1;
+drop eth2;
+drop eth3;
+drop eth4;
