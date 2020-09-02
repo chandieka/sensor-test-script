70bbb45e6efccb73defac6c400bfe72c81823a99
chandieka <candiekatasbihyantra2@gmail.com>
2020-09-02 07:05:32 -0400

    update

--- data-lost.sh
+++ data-lost.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+int="eth1"
+
 getDrop() {
     dropPacket=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$1/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
     echo $dropPacket;
@@ -9,3 +11,20 @@ getCapture(){
     capPacket=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$1/stats.log | grep capture | tail -n 1 | cut -d '|' -f3);
     echo $capPacket;
 }
+
+oldCap=0
+oldLost=0
+
+while true;
+do 
+newCap=$(getCap() $int)
+newLost=$(getCap() $int)
+
+cap=$((oldCap - newCap));
+lost=$((oldLost - newLost));
+
+echo "$cap $lost";
+
+oldCap=oldCap
+newCap=newCap
+done;
