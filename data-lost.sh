9729791e9a172414c8bcb67aab6c3a2268f95fca
chandieka <candiekatasbihyantra2@gmail.com>
2020-09-02 08:37:46 -0400

    update

--- data-lost.sh
+++ data-lost.sh
@@ -1,15 +1,22 @@
 #! /bin/bash
-
 int="eth1"
 
 getDrop() {
-    dropPacket=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$1/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
-    echo $dropPacket;
+    dropPacket1=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$1/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
+    dropPacket2=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$2/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
+    dropPacket3=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$3/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
+    dropPacket4=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$4/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
+
+    echo $(( dropPacket1 + dropPacket2 + dropPacket3 + dropPacket4 ));
 }
 
 getCapture(){
-    capPacket=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$1/stats.log | grep capture | tail -n 1 | cut -d '|' -f3);
-    echo $capPacket;
+    capPacket1=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$1/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
+    capPacket2=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$2/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
+    capPacket3=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$3/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
+    capPacket4=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$4/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
+
+    echo $(( capPacket1 + capPacket2 + capPacket3 + capPacket4 )) ;
 }
 
 oldCap=0
@@ -17,18 +24,18 @@ oldLost=0
 
 while true;
 do 
-newCap=$(getCapture $int)
-newLost=$(getLost $int)
+    newCap=$(getCapture $int)
+    newLost=$(getDrop $int)
 
-cap=$((newCap - oldCap));
-lost=$((newDrop - oldDrop));
+    cap=$((newCap - oldCap));
+    lost=$((newLost - oldLost);
 
-if [[ $cap != 0 && $lost != 0 ]]
-then
-    echo "$cap $lost";
-fi;
+    if [[ $cap != 0 && $lost != 0 ]]
+    then
+        echo "$cap $lost";
+    fi;
 
-oldCap=newCap
-newCap=newOld
-sleep 1;
+    oldCap=$newCap
+    newLost=$newLost
+    sleep 1;
 done;
