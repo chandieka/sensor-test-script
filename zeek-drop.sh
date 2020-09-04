c2cf0857d4378e98c523e402e99acd61989c4cc1
chandieka <candiekatasbihyantra2@gmail.com>
2020-09-04 04:01:35 -0400

    update

--- zeek-drop.sh
+++ zeek-drop.sh
@@ -4,7 +4,9 @@ while true;
 do
     newZeekDrop=$(cat /nsm/bro/logs/stats/stats.log | grep "total" | grep "nic_drops" | tail -n1 | cut -d ' ' -f 5);
     newZeekPkts=$(cat /nsm/bro/logs/stats/stats.log | grep "total" | grep "nic_pkts" | tail -n1 | cut -d ' ' -f 5);
-    newZeekTotal=$( echo "scale=4; $newZeekPkts+$newZeekDrop" | bc );
+    a=$(cat /nsm/bro/logs/stats/stats.log | grep "total" | grep "nic_pkts" | paste -sd+ | bc )
+    b=$(cat /nsm/bro/logs/stats/stats.log | grep "total" | grep "nic_drops" | paste -sd+ | bc)
+    newZeekTotal=$( echo "scale=4; $a+$b" );
 
     echo "$newZeekDrop $newZeekPkts $newZeekTotal"
     sleep 1;
