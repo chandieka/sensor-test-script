4201f034e1c3887b03b74857daadfdd2c272242d
chandieka <candiekatasbihyantra2@gmail.com>
2020-09-04 03:43:07 -0400

    update

--- zeek-drop.sh
+++ zeek-drop.sh
@@ -6,6 +6,6 @@ do
     newZeekPkts=$(cat /nsm/bro/logs/stats/stats.log | grep "total" | grep "nic_pkts" | tail -n1 | cut -d ' ' -f 5);
     newZeekTotal=$( echo "scale=4; $newZeekPkts+$newZeekDrop" | bc );
 
-    echo "newZeelDrop newZeekPkts newZeekTotal"
+    echo "$newZeelDrop $newZeekPkts $newZeekTotal"
     sleep 1;
 done;
