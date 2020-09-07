3cde732b9624ebc46d1511abb92131c0cdb9b251
chandieka <candiekatasbihyantra2@gmail.com>
2020-09-07 08:08:53 -0400

    update

--- get-data.sh
+++ get-data.sh
@@ -4,4 +4,3 @@ mkdir ~/pcap/
 cd ~/pcap/
 wget http://downloads.digitalcorpora.org/corpora/packets/5gb-tcp-connection.pcap.gz
 gunzip -v ./5gb-tcp-connection.pcap.gz
-
--- traffic-replay.sh
+++ traffic-replay.sh
@@ -1,16 +1,3 @@
 # /bin/bash
-captime=$((60*60)) # time
-int="eth1" # interface
-mbps="10000" # network speed
 
-# absolute path for pcap
-pcap1=""
-pcap2=""
-pcap3=""
-pcap4=""
-
-sudo timeout captime tcpreplay -i $int -K --loop 10000 --mbps $mbps $pcap1 &;
-sudo timeout captime tcpreplay -i $int -K --loop 10000 --mbps $mbps $pcap1 &; 
-sudo timeout captime tcpreplay -i $int -K --loop 10000 --mbps $mbps $pcap1 &; 
-sudo timeout captime tcpreplay -i $int -K --loop 10000 --mbps $mbps $pcap1 &; 
-sudo timeout captime tcpreplay -i $int -K --loop 10000 --mbps $mbps $pcap1 &; 
+sudo tcpreplay -K -t --unique-ip -i eth1 -l 1000 5gb-tcp-connection.pcap
