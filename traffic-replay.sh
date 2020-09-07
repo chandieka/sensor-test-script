8df57d6d2f81426a2aa2cb92c4ac9ff960db572f
chandieka <candiekatasbihyantra2@gmail.com>
2020-09-07 08:15:27 -0400

    update

--- traffic-replay.sh
+++ traffic-replay.sh
@@ -1,3 +1,4 @@
 # /bin/bash
 
-sudo tcpreplay -K -t --unique-ip -i eth1 -l 1000 ~/pcap/5gb-tcp-connection.pcap
+sudo tcpreplay -K -t --unique-ip -i eth1 -l 1000 ~/pcap/5gb-tcp-connection.pcap &
+nload eth1
