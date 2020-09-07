c290b2ee999ad46e9ff2097b77df9d01d6ed7e15
chandieka <candiekatasbihyantra2@gmail.com>
2020-09-07 09:40:36 -0400

    update

--- data-lost.sh
+++ data-lost.sh
@@ -3,25 +3,28 @@ capint=$1
 dest=$2
 
 getDrop() {
-    dropPacket1=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$1/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
-    dropPacket2=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$2/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
-    dropPacket3=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$3/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
-    dropPacket4=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$4/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
-    dropPacket5=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$5/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
-    dropPacket6=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$6/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
+    # dropPacket1=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$1/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
+    # dropPacket2=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$2/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
+    # dropPacket3=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$3/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
+    # dropPacket4=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$4/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
+    # dropPacket5=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$5/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
+    # dropPacket6=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$6/stats.log | grep drops | tail -n 1 | cut -d '|' -f3);
 
-    echo $(( dropPacket1 + dropPacket2 + dropPacket3 + dropPacket4 + dropPacket5  + dropPacket6 ));
+    # echo $(( dropPacket1 + dropPacket2 + dropPacket3 + dropPacket4 + dropPacket5  + dropPacket6 ));\
+    echo dropPacket1;
+
 }
 
 getCapture(){
     capPacket1=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$1/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
-    capPacket2=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$2/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
-    capPacket3=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$3/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
-    capPacket4=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$4/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
-    capPacket5=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$5/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
-    capPacket6=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$6/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
+    # capPacket2=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$2/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
+    # capPacket3=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$3/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
+    # capPacket4=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$4/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
+    # capPacket5=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$5/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
+    # capPacket6=$(cat /nsm/sensor_data/virtualsocdemo-sensor-$6/stats.log | grep packets | tail -n 1 | cut -d '|' -f3);
 
-    echo $(( capPacket1 + capPacket2 + capPacket3 + capPacket4 + capPacket5  + capPacket6 )) ;
+    # echo $(( capPacket1 + capPacket2 + capPacket3 + capPacket4 + capPacket5  + capPacket6 )) ;
+    echo capPacket1;
 }
 
 oldCap=0;
--- mon-net.sh
+++ mon-net.sh
@@ -17,52 +17,55 @@ printf "$header" "COUNTER" "DATE" "TIMESTAMP" "TX(Mbps)" "RX(Mbps)" >> ./$dest/n
 while [[ $counter -le $capint ]];
 do
     txbytes_old1="`cat /sys/class/net/$int1/statistics/tx_bytes`";
-    txbytes_old2="`cat /sys/class/net/$int2/statistics/tx_bytes`";
-    txbytes_old3="`cat /sys/class/net/$int3/statistics/tx_bytes`";
-    txbytes_old4="`cat /sys/class/net/$int4/statistics/tx_bytes`";
-    txbytes_old5="`cat /sys/class/net/$int5/statistics/tx_bytes`";
-    txbytes_old6="`cat /sys/class/net/$int6/statistics/tx_bytes`";
+    # txbytes_old2="`cat /sys/class/net/$int2/statistics/tx_bytes`";
+    # txbytes_old3="`cat /sys/class/net/$int3/statistics/tx_bytes`";
+    # txbytes_old4="`cat /sys/class/net/$int4/statistics/tx_bytes`";
+    # txbytes_old5="`cat /sys/class/net/$int5/statistics/tx_bytes`";
+    # txbytes_old6="`cat /sys/class/net/$int6/statistics/tx_bytes`";
 
     rxbytes_old1="`cat /sys/class/net/$int1/statistics/rx_bytes`";
-    rxbytes_old2="`cat /sys/class/net/$int2/statistics/rx_bytes`";
-    rxbytes_old3="`cat /sys/class/net/$int3/statistics/rx_bytes`";
-    rxbytes_old4="`cat /sys/class/net/$int4/statistics/rx_bytes`";
-    rxbytes_old5="`cat /sys/class/net/$int5/statistics/rx_bytes`";
-    rxbytes_old6="`cat /sys/class/net/$int6/statistics/rx_bytes`";
+    # rxbytes_old2="`cat /sys/class/net/$int2/statistics/rx_bytes`";
+    # rxbytes_old3="`cat /sys/class/net/$int3/statistics/rx_bytes`";
+    # rxbytes_old4="`cat /sys/class/net/$int4/statistics/rx_bytes`";
+    # rxbytes_old5="`cat /sys/class/net/$int5/statistics/rx_bytes`";
+    # rxbytes_old6="`cat /sys/class/net/$int6/statistics/rx_bytes`";
     
     sleep 1;
     
     txbytes_new1="`cat /sys/class/net/$int1/statistics/tx_bytes`";
-    txbytes_new2="`cat /sys/class/net/$int2/statistics/tx_bytes`";
-    txbytes_new3="`cat /sys/class/net/$int3/statistics/tx_bytes`";
-    txbytes_new4="`cat /sys/class/net/$int4/statistics/tx_bytes`";
-    txbytes_new5="`cat /sys/class/net/$int5/statistics/tx_bytes`";
-    txbytes_new6="`cat /sys/class/net/$int6/statistics/tx_bytes`";
+    # txbytes_new2="`cat /sys/class/net/$int2/statistics/tx_bytes`";
+    # txbytes_new3="`cat /sys/class/net/$int3/statistics/tx_bytes`";
+    # txbytes_new4="`cat /sys/class/net/$int4/statistics/tx_bytes`";
+    # txbytes_new5="`cat /sys/class/net/$int5/statistics/tx_bytes`";
+    # txbytes_new6="`cat /sys/class/net/$int6/statistics/tx_bytes`";
 
     rxbytes_new1="`cat /sys/class/net/$int1/statistics/rx_bytes`";
-    rxbytes_new2="`cat /sys/class/net/$int2/statistics/rx_bytes`";
-    rxbytes_new3="`cat /sys/class/net/$int3/statistics/rx_bytes`";
-    rxbytes_new4="`cat /sys/class/net/$int4/statistics/rx_bytes`";
-    rxbytes_new5="`cat /sys/class/net/$int6/statistics/rx_bytes`";
-    rxbytes_new6="`cat /sys/class/net/$int5/statistics/rx_bytes`";
+    # rxbytes_new2="`cat /sys/class/net/$int2/statistics/rx_bytes`";
+    # rxbytes_new3="`cat /sys/class/net/$int3/statistics/rx_bytes`";
+    # rxbytes_new4="`cat /sys/class/net/$int4/statistics/rx_bytes`";
+    # rxbytes_new5="`cat /sys/class/net/$int6/statistics/rx_bytes`";
+    # rxbytes_new6="`cat /sys/class/net/$int5/statistics/rx_bytes`";
 
 
     txbytes_total1=$(( txbytes_new1- txbytes_old1));
-    txbytes_total2=$(( txbytes_new2- txbytes_old2));
-    txbytes_total3=$(( txbytes_new3- txbytes_old3));
-    txbytes_total4=$(( txbytes_new4- txbytes_old4));
-    txbytes_total5=$(( txbytes_new5- txbytes_old5));
-    txbytes_total6=$(( txbytes_new6- txbytes_old6));
+    # txbytes_total2=$(( txbytes_new2- txbytes_old2));
+    # txbytes_total3=$(( txbytes_new3- txbytes_old3));
+    # txbytes_total4=$(( txbytes_new4- txbytes_old4));
+    # txbytes_total5=$(( txbytes_new5- txbytes_old5));
+    # txbytes_total6=$(( txbytes_new6- txbytes_old6));
 
     rxbytes_total1=$(( rxbytes_new1 - rxbytes_old1));
-    rxbytes_total2=$(( rxbytes_new2 - rxbytes_old2));
-    rxbytes_total3=$(( rxbytes_new3 - rxbytes_old3));
-    rxbytes_total4=$(( rxbytes_new4 - rxbytes_old4));
-    rxbytes_total5=$(( rxbytes_new5 - rxbytes_old5));
-    rxbytes_total6=$(( rxbytes_new6 - rxbytes_old6));
+    # rxbytes_total2=$(( rxbytes_new2 - rxbytes_old2));
+    # rxbytes_total3=$(( rxbytes_new3 - rxbytes_old3));
+    # rxbytes_total4=$(( rxbytes_new4 - rxbytes_old4));
+    # rxbytes_total5=$(( rxbytes_new5 - rxbytes_old5));
+    # rxbytes_total6=$(( rxbytes_new6 - rxbytes_old6));
 
-    txbytes_sum=$((txbytes_total1 + txbytes_total2 + txbytes_total3 + txbytes_total4 + txbytes_total5 + txbytes_total6))
-    rxbytes_sum=$((rxbytes_total1 + rxbytes_total2 + rxbytes_total3 + rxbytes_total4 + rxbytes_total5 + rxbytes_total6))
+    # txbytes_sum=$((txbytes_total1 + txbytes_total2 + txbytes_total3 + txbytes_total4 + txbytes_total5 + txbytes_total6))
+    # rxbytes_sum=$((rxbytes_total1 + rxbytes_total2 + rxbytes_total3 + rxbytes_total4 + rxbytes_total5 + rxbytes_total6))
+   
+    txbytes_sum=$((txbytes_total1))
+    rxbytes_sum=$((rxbytes_total1))
     
     rxmbps=$(echo "scale=2; $rxbytes_sum*8/1000000" | bc);
     txmbps=$(echo "scale=2; $txbytes_sum*8/1000000" | bc);
